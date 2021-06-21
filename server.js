63f1f27867283af74dea089191c6ef152c9c8303
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-21 22:12:12 +0300

    123

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ server.get('/books', BookHandler);
 
 ////////////////////////////////////////////////mongoose////////////////////////
 
-const MONGO_URL = process.env.MONGO_URL;
-
-mongoose.connect(`${MONGO_URL}/test`, {
+mongoose.connect('mongodb://localhost:27017/test', {
     useNewUrlParser: true,
      useUnifiedTopology: true,
 });
