9493ce62ad56374ebf0d6bf693181b02bee7125c
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-22 22:32:06 +0300

    done lab13

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const BookHandler = require('./Modules/mangoose-book');
+const addBookHandler = require('./Modules/addBookHandler');
 
 const server = express();
 server.use(cors());
-
+server.use(express.json());
 const PORT = process.env.PORT;
 
 ///////////////////////////////////////////routes////////////////////////
@@ -16,75 +17,18 @@ const PORT = process.env.PORT;
 server.get('/', HomePage);
 
 //localhost:3001/books
-server.get('/books', BookHandler);
+server.get('/books', BookHandler.BookHandler);
 
+server.post('/addBooks', addBookHandler);
 
-////////////////////////////////////////////////mongoose////////////////////////
-
-mongoose.connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true,
-     useUnifiedTopology: true,
-});
-
-// schemas 
-
-const book = new mongoose.Schema({
-    name: String,
-    desc: String,
-    img: String,
-});
-
-const owner = new mongoose.Schema({
-    email: String,
-    books: [book],
-    
-});
-
-// modals
-
-const Owners = mongoose.model('Book', owner);
-
-// functions
-
-function seeOwnersCollection (){
-    const Nadeen = new Owners ({
-        email: 'shwueihat16@gmail.com',
-        books: [
-            {
-                name: 'book1',
-                desc: 'This book is Amazing',
-                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFlAMxLcCmEsYsPzbbaSBzSPrzIRjRS5E2Zw&usqp=CAU'
-            },
-            {
-                name: 'book2',
-                desc: 'This book will make you great',
-                img: 'https://img.themanual.com/image/themanual/bed-2-416x416.jpg'
-            },
-        ],
-        
-    });
-    Nadeen.save();
-}
-
-// seeOwnersCollection();
-
-/////////////////////////////////////////////////////////functions///////////////////////////
-
+server.delete('/deletebooks/:id', deletebooksHandler)
 
 function HomePage (req,res){
     res.send('Home Page');
 };
 
-function BookHandler(req,res){
-    let email = req.query.email;
 
-    Owners.find({ email : email },(err, data) =>{
-        err ? console.log('There is an error') : res.send(data[0].books); 
-            
-        
-    });
 
-}
 ///////////////////////////////////////////////////////////////listen/////////////////////////
 
 server.listen(PORT,() => {
